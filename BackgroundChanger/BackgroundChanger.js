a7e9c4604ba40bddf4e0a63a0dc7ccc4c3b6b5cb
stupid cat <kirkbenm@gmail.com>
2017-04-25 18:26:02 -0600

    BackgroundChanger

---
+++ BackgroundChanger/BackgroundChanger.js
@@ -0,0 +1,72 @@
+const Plugin = require('../Structures/Plugin');
+
+class BackgroundChanger extends Plugin {
+    constructor() {
+        super({
+            author: 'stupid cat',
+            version: '1.0.0',
+            description: 'Changes background images on an interval',
+            color: '3622a1'
+        });
+
+        this.log('Injecting plugin CSS');
+        this.styleTag = document.createElement('style');
+        this.styleTag.innerHTML = `/** Remove a background discord put in front of the body **/
+#app-mount > div > div.app.flex-vertical.theme-dark > div.layers.flex-vertical.flex-spacer,
+#app-mount > div > div:nth-child(2) > div > div.layers.flex-vertical.flex-spacer,
+.theme-dark .layer, .theme-dark .layers {
+	background: rgba(0, 0, 0, 0) !important;
+}
+
+/** Set up animated transitions **/
+body {
+    -webkit-transition: background 3s ease-in-out;
+    transition: background 3s ease-in-out;
+    background-size: cover;
+}`;
+        document.head.appendChild(this.styleTag);
+
+        // Empty starting URL
+        this.lastUrl = '';
+
+        // Switch every 10 minutes
+        setInterval(this.next.bind(this), 10 * 60 * 1000);
+        this.next();
+        this.log('Rotater initialized.');
+    }
+
+    get urls() {
+        return [
+            'your',
+            'urls',
+            'here'
+        ];
+    }
+
+    get url() {
+        let url;
+        do {
+            url = this.urls[Math.floor(Math.random() * this.urls.length)];
+        } while (url == this.lastUrl);
+        this.lastUrl = url;
+        return url;
+    }
+
+    next() {
+        this.log('Changing background...');
+        let url = this.url;
+        document.getElementById('app-mount').style.backgroundImage = 'url(' + url + ')';
+        setTimeout(function () {
+            document.getElementsByTagName('body')[0].style.backgroundImage = 'url(' + url + ')';
+        }, 200);
+    }
+
+    unload() {
+        this.log('Killing interval');
+        clearInterval(this.interval);
+        this.log('Un-injecting CSS');
+        document.head.removeChild(this.styleTag);
+    }
+}
+
+module.exports = BackgroundChanger;
---
+++ BackgroundChanger/readme.md
@@ -0,0 +1,7 @@
+# BackgroundChanger
+
+You should place this file in the `Plugins` folder of the DiscordInjections project.
+
+This switches your background every 10 minutes to a random URL in the pool.
+
+Please populate the `get urls()` with your own URLs. They must use https!
