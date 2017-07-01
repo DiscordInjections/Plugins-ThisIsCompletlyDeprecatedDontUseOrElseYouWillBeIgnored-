008f5a7babcc32de4bb2596e1255ed4e1ac3ebfe
martmists <mail@martmists.com>
2017-07-01 12:04:07 +0000

    Update EmotePlugin.js

--- Emotes/EmotePlugin.js
+++ Emotes/EmotePlugin.js
@@ -1,6 +1,7 @@
 const Plugin = module.parent.require('../Structures/Plugin');
 
 const TwitchEmotes = require('./emoteData_Twitch.json');
+const BTTVEmotes = require('./emoteData_BTTV.json');
 
 
 class EmotePlugin extends Plugin {
@@ -12,19 +13,41 @@ class EmotePlugin extends Plugin {
             color: 'CA4CE3'
         });
 
+        this.disableBTTV = true
+
         setInterval(this.checkEmotes.bind(this), 15000);
+        this.emotes = Object.keys(TwitchEmotes);
+        this.bttv = Object.keys(BTTVEmotes);
     }
 
     checkEmotes(){
-        let emotes = Object.keys(TwitchEmotes);
+        let emotes = this.emotes
+        let bttv = this.bttv
         let msgs = document.getElementsByClassName("markup");
+
         for (let m=0; m<msgs.length; m++){
             let e = msgs[m];
             for (let i=0; i<emotes.length; i++){
-                if (e.innerHTML.indexOf(emotes[i]) != -1){
+                if (e.innerText.indexOf(emotes[i]+" ") != -1 ||
+                        e.innerText.indexOf(" "+emotes[i]) != -1 ||
+                        e.innerText == emotes[i]){
+                    this.log("Adding emote: "+emotes[i]);
                     e.innerHTML = e.innerHTML.replace(
                         emotes[i],
-                        "<img src='https://static-cdn.jtvnw.net/emoticons/v1/"+TwitchEmotes[emotes[i]]+"/1.0' class='emote'/>");
+                        "<img src='https://static-cdn.jtvnw.net/emoticons/v1/"+TwitchEmotes[emotes[i]]+"/1.0' class='emote' alt='"+emotes[i]+"'/>");
+                }
+            };
+
+            if (!this.disableBTTV){
+                for (let i=0; i<bttv.length; i++){
+                    if (e.innerText.indexOf(bttv[i]+" ") != -1 ||
+                            e.innerText.indexOf(" "+bttv[i]) != -1 ||
+                            e.innerText == bttv[i]){
+                        this.log("Adding emote: "+bttv[i]);
+                        e.innerHTML = e.innerHTML.replace(
+                            bttv[i],
+                            "<img src='https://cdn.betterttv.net/emote/"+BTTVEmotes[bttv[i]]+"/1x' class='emote' alt='"+bttv[i]+"'/>");
+                    }
                 }
             }
         }
