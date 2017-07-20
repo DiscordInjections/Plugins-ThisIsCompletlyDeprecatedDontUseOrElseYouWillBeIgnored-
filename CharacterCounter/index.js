09d2a27950453a5dff5742dee3199b4d694a54f3
Snazzah <SnazzyPine25@users.noreply.github.com>
2017-07-20 06:21:21 -0500

    Add DoubleClickEdit & CharacterCounter

---
+++ CharacterCounter/index.js
@@ -0,0 +1,43 @@
+const Plugin = module.parent.require('../Structures/Plugin');
+const $ = require("jquery");
+
+class CharacterCounter extends Plugin {
+    constructor(...args) {
+        super(...args);
+        this.didInit = false;
+        this.mo = new MutationObserver(this.init.bind(this));
+        this.mo.observe(document.querySelector("[data-reactroot]"), { childList: true, subtree: true });
+    }
+
+    init() {
+        if(document.querySelector("#charcounter")) return;
+        let ta = document.querySelector(".channel-text-area-default");
+        if(!ta) return;
+        this.log("Initializing Counter...");
+        let eventhandler = e => {
+            let length = e.target.value.length;
+            $("#charcounter").text(`${length}/2000`);
+            if(length > 1999){
+                $("#charcounter")[0].className = "limit"
+            }else if(length > 1899){
+                $("#charcounter")[0].className = "danger"
+            }else if(length > 1499){
+                $("#charcounter")[0].className = "warning"
+            }else if(length > 999){
+                $("#charcounter")[0].className = "minor"
+            }else{
+                $("#charcounter")[0].className = ""
+            }
+        };
+        $(".channel-text-area-default").append(`<span id="charcounter">${document.querySelector(".chat .content textarea").value.length}/2000</span>`);
+        $(".chat .content textarea").off("input.charCounter").off("keydown.charCounterKey").on("input.charCounter", eventhandler).on("keydown.charCounterKey", eventhandler);
+    }
+
+    unload() {
+        this.mo.disconnect();
+        if(document.querySelector("#charcounter")) $("#charcounter").remove();
+        $(".chat .content textarea").off("input.charCounter").off("keydown.charCounterKey");
+    }
+}
+
+module.exports = CharacterCounter;
---
+++ DoubleClickEdit/README.md
@@ -0,0 +1,6 @@
+# DoubleClickEdit
+###### [Originally made by Jiiks](https://github.com/Jiiks/BetterDiscordApp/blob/master/Plugins/dblClickEdit.plugin.js)
+
+You should place this file in the `Plugins` folder of the DiscordInjections project.
+
+This plugin lets you double click messages to edit them.
