465329af66fc8b3850a376b32d0e23a1ff3c2819
Snazzah <SnazzyPine25@users.noreply.github.com>
2017-07-22 03:30:10 -0500

    Add a delay to reading textarea bc idk why

--- CharacterCounter/index.js
+++ CharacterCounter/index.js
@@ -15,28 +15,30 @@ class CharacterCounter extends Plugin {
         if(!ta) return;
         this.log("Initializing Counter...");
         let eventhandler = e => {
-            let length = e.target.value.length;
-            $("#charcounter").text(`${length}/2000`);
-            if(length > 1999){
-                $("#charcounter")[0].className = "limit"
-            }else if(length > 1899){
-                $("#charcounter")[0].className = "danger"
-            }else if(length > 1499){
-                $("#charcounter")[0].className = "warning"
-            }else if(length > 999){
-                $("#charcounter")[0].className = "minor"
-            }else{
-                $("#charcounter")[0].className = ""
-            }
+            setTimeout(() => {
+                let length = e.target.value.length;
+                $("#charcounter").text(`${length}/2000`);
+                if(length > 1999){
+                    $("#charcounter")[0].className = "limit"
+                }else if(length > 1899){
+                    $("#charcounter")[0].className = "danger"
+                }else if(length > 1499){
+                    $("#charcounter")[0].className = "warning"
+                }else if(length > 999){
+                    $("#charcounter")[0].className = "minor"
+                }else{
+                    $("#charcounter")[0].className = ""
+                }
+            }, 100);
         };
         $(".channel-text-area-default").append(`<span id="charcounter">${document.querySelector(".chat .content textarea").value.length}/2000</span>`);
-        $(".chat .content textarea").off("input.charCounter").off("keydown.charCounterKey").on("input.charCounter", eventhandler).on("keydown.charCounterKey", eventhandler);
+        $(".chat .content textarea").off("keypress.charCounter").off("keydown.charCounterKey").on("keypress.charCounter", eventhandler).on("keydown.charCounterKey", eventhandler);
     }
 
     unload() {
         this.mo.disconnect();
         if(document.querySelector("#charcounter")) $("#charcounter").remove();
-        $(".chat .content textarea").off("input.charCounter").off("keydown.charCounterKey");
+        $(".chat .content textarea").off("keypress.charCounter").off("keydown.charCounterKey");
     }
 }
 
