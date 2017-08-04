94e7f77b7f33744ff5693b38fd5297e62e722277
Kev <19242043+kev1ns@users.noreply.github.com>
2017-08-04 18:43:33 -0400

    fixes to ssh plugin (#36)
    * fixes to ssh plugin
    
    * Update exec.js

--- SSH/Commands/ssh.js
+++ SSH/Commands/ssh.js
@@ -4,13 +4,25 @@ let config = require('../config.json');
 
 module.exports = {
 	info: "Create an SSH connection",
-	usage: "none",
+	usage: "",
 	func: args => {
 		if(window.DI.ssh) {
 			window.DI.ssh.dispose();
 			window.DI.Helpers.sendLog('SSH', 'Closing current connection and opening new one...', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Octicons-terminal.svg/2000px-Octicons-terminal.svg.png')
 		};
-		if(config.privateKey) config.privateKey = require('fs').readFileSync(config.privateKey).catch(() => {return new Error('Cannot find privateKey file')});
+		if(config.privateKey) {
+			try {
+				config.privateKey = require('fs').readFileSync(config.privateKey).catch(() => {return new Error('Cannot find privateKey file')});
+				delete config['password'];
+			} catch(e) {
+				delete config['privateKey'];
+				window.console.error("[ssh] Could not find privateKey in path. Attempting to use password..");
+			}
+		};
+		if(config.password && config.privateKey) {
+			delete config['privateKey'];
+		};
+
 		ssh.connect(config).then(() => {
 			window.DI.ssh = ssh;
 			window.DI.Helpers.sendLog('SSH', 'Successfully created the SSH connection. Use the `exec` command to execute SSH commands.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Octicons-terminal.svg/2000px-Octicons-terminal.svg.png')
