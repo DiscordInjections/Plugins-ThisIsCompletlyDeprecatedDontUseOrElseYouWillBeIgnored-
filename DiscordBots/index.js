07658c00f89f5e3c2c7d4e71fb940017257ec502
Snazzah <SnazzyPine25@users.noreply.github.com>
2017-08-09 13:38:32 -0500

    Use DI's settings tab

--- DiscordBots/index.js
+++ DiscordBots/index.js
@@ -4,6 +4,8 @@ const request = require("superagent");
 const cm = require('cache-manager');
 const Discord = require('discord.js');
 const cache = cm.caching({store: 'memory', db: 0, ttl: 3600});
+const e = window.DI.React.createElement;
+const { SettingsDivider, SettingsOptionToggle, SettingsOptionTextbox, SettingsOptionDescription } = window.DI.require('./Structures/Components');
 
 Number.prototype.formatNumber = function(){
     return this.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
@@ -18,26 +20,24 @@ Object.defineProperty(Discord.User.prototype, 'avatarURL', {
 class DiscordBots extends Plugin {
     constructor(...args) {
         super(...args);
-        this.optionHTML = $(`<div class="db-options"><h2 class="h2-2ar_1B title-1pmpPr size16-3IvaX_ height20-165WbF weightSemiBold-T8sxWH defaultColor-v22dK1 margin-top-60 margin-bottom-20">DiscordBots Plugin</h2><div class="flex-lFgbSz flex-3B1Tl4 vertical-3X17r5 flex-3B1Tl4 directionColumn-2h-LPR justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO switchItem-1uofoz marginBottom20-2Ifj-2" style="flex: 1 1 auto;"><div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO" style="flex: 1 1 auto;"><h3 class="h3-gDcP8B title-1pmpPr marginReset-3hwONl size16-3IvaX_ height24-2pMcnc weightMedium-13x9Y8 defaultColor-v22dK1 title-3i-5G_ marginReset-3hwONl flexChild-1KGW5q" style="flex: 1 1 auto;">Enabled</h3><div class="switchWrapperDefaultActive-2IdHq2 switchWrapperDefault-3GSsCS switchWrapper-3sSQdm flexChild-1KGW5q" style="flex: 0 0 auto;"><input type="checkbox" class="checkbox-1KYsPm db-enabled" value="on"><div class="switch-3lyafC"></div></div></div><div class="divider-1G01Z9 divider-2nTTsf marginTop20-3UscxH"></div></div><div class="flex-lFgbSz flex-3B1Tl4 vertical-3X17r5 flex-3B1Tl4 directionColumn-2h-LPR justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO switchItem-1uofoz marginBottom20-2Ifj-2" style="flex: 1 1 auto;"><div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO" style="flex: 1 1 auto;"><h3 class="h3-gDcP8B title-1pmpPr marginReset-3hwONl size16-3IvaX_ height24-2pMcnc weightMedium-13x9Y8 defaultColor-v22dK1 title-3i-5G_ marginReset-3hwONl flexChild-1KGW5q" style="flex: 1 1 auto;">Show information in user pop-outs</h3><div class="switchWrapperDefaultActive-2IdHq2 switchWrapperDefault-3GSsCS switchWrapper-3sSQdm flexChild-1KGW5q" style="flex: 0 0 auto;"><input type="checkbox" class="checkbox-1KYsPm db-popout" value="on"><div class="switch-3lyafC"></div></div></div><div class="divider-1G01Z9 divider-2nTTsf marginTop20-3UscxH"></div></div><div class="flex-lFgbSz flex-3B1Tl4 vertical-3X17r5 flex-3B1Tl4 directionColumn-2h-LPR justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO switchItem-1uofoz marginBottom20-2Ifj-2" style="flex: 1 1 auto;"><h3 class="h3-gDcP8B title-1pmpPr marginReset-3hwONl size16-3IvaX_ height24-2pMcnc weightMedium-13x9Y8 defaultColor-v22dK1 title-3i-5G_ marginReset-3hwONl flexChild-1KGW5q" style="flex: 1 1 auto;">Enable full description rendering</h3><div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO" style="flex: 1 1 auto;"><div class="description-3MVziF formText-1L-zZB note-UEZmbY marginTop4-2rEBfJ modeDefault-389VjU primary-2giqSn" style="flex: 1 1 auto;">Descriptions are rendered in a iframe element. This option is turned off by default to lessen loading unneeded assets.</div><div class="switchWrapperDefaultActive-2IdHq2 switchWrapperDefault-3GSsCS switchWrapper-3sSQdm flexChild-1KGW5q" style="flex: 0 0 auto;"><input type="checkbox" class="checkbox-1KYsPm db-renderdesc" value="on"><div class="switch-3lyafC"></div></div></div><div class="divider-1G01Z9 divider-2nTTsf marginTop20-3UscxH"></div></div><div class="flex-lFgbSz flex-3B1Tl4 vertical-3X17r5 flex-3B1Tl4 directionColumn-2h-LPR justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO switchItem-1uofoz marginBottom20-2Ifj-2" style="flex: 1 1 auto;"><div class="flex-lFgbSz flex-3B1Tl4 horizontal-2BEEBe horizontal-2VE-Fw flex-3B1Tl4 directionRow-yNbSvJ justifyStart-2yIZo0 alignStretch-1hwxMa noWrap-v6g9vO" style="flex: 1 1 auto;"><h3 class="h3-gDcP8B title-1pmpPr marginReset-3hwONl size16-3IvaX_ height24-2pMcnc weightMedium-13x9Y8 defaultColor-v22dK1 title-3i-5G_ marginReset-3hwONl flexChild-1KGW5q" style="flex: 1 1 auto;">Use DiscordBots.org instead of bots.discord.pw</h3><div class="switchWrapperDefaultActive-2IdHq2 switchWrapperDefault-3GSsCS switchWrapper-3sSQdm flexChild-1KGW5q" style="flex: 0 0 auto;"><input type="checkbox" class="checkbox-1KYsPm db-usedbl" value="on"><div class="switch-3lyafC"></div></div></div><div class="description-3MVziF formText-1L-zZB note-UEZmbY marginTop4-2rEBfJ modeDefault-389VjU primary-2giqSn" style="flex: 1 1 auto;"><a href="//discordbots.org" target="_blank">DiscordBots.org</a> uses a similar API as <a href="//bots.discord.pw" target="_blank">bots.discord.pw</a> but adds upvotes, badges for certified developers and bots and does not require a token.</div></div><input class="inputDefault-Y_U37D input-2YozMi size16-3IvaX_" name="password" value="" placeholder="bots.discord.pw Token" maxlength="999" type="password"><div class="divider-1G01Z9 divider-2nTTsf marginTop20-3UscxH"></div></div>`);
         this.belowNoteHTML = "";
         this.checkedProfile = false;
         this.noProfile = true;
         this.checkedPopout = false;
         this.noPopout = true;
         this.mo = new MutationObserver((changes, _) => {
-            this.injectOptionHTML();
             this.checkForProfile();
             this.checkForPopout();
         });
-        this.load();
         this.mo.observe($("[data-reactroot]")[0], { childList: true, subtree: true });
         this.registerCommand({
             name: "db-setbdptoken",
             info: "Sets your bots.discord.pw token.",
             usage: "<token>",
             func: (args) => {
-                this.settings.bdptoken = args[0] || "";
-                this.save();
+                let settings = this.settings;
+                settings.bdptoken = args[0] || "";
+                this.settings = settings;
                 if(args[0] || args[0] === "") window.DI.Helpers.sendLog('Discord Bots Plugin', "Reset bots.discord.pw token.", this.iconURL);
                     else window.DI.Helpers.sendLog('Discord Bots Plugin', "Set bots.discord.pw token.", this.iconURL);
             }
@@ -48,12 +48,13 @@ class DiscordBots extends Plugin {
             usage: "<bdp|dbl>",
             func: (args) => {
                 if(!args[0]) args[0] = "";
+                let settings = this.settings;
                 switch(args[0].toLowerCase()){
                     case "bdp":
                     case "bots.discord.pw":
                     case "botsdiscordpw":
-                        this.settings.usedbl = false;
-                        this.save();
+                        settings.usedbl = false;
+                        this.settings = settings;
                         window.DI.Helpers.sendLog('Discord Bots Plugin', "Mode set to bots.discord.pw.", this.iconURL);
                         break;
                     case "dbl":
@@ -61,8 +62,8 @@ class DiscordBots extends Plugin {
                     case "discordbots.org":
                     case "discordbotsorg":
                     case "dborg":
-                        this.settings.usedbl = true;
-                        this.save();
+                        settings.usedbl = true;
+                        this.settings = settings;
                         window.DI.Helpers.sendLog('Discord Bots Plugin', "Mode set to Discordbots.org.", this.iconURL);
                         break;
                     default:
@@ -71,6 +72,7 @@ class DiscordBots extends Plugin {
                 }
             }
         });
+        window.DI.DISettings.registerSettingsTab(this, 'Discord Bots', DiscordBotsSettings);
     }
 
     get configTemplate() {
@@ -85,15 +87,6 @@ class DiscordBots extends Plugin {
 
     unload() {
         this.mo.disconnect();
-    }
-
-    load() {
-        this.settings = $.extend({}, {enabled:true,usedbl:false,renderdesc:false,bdptoken:"",popout:false}, JSON.parse(window.DI.localStorage.getItem("DiscordBotsPlugin")));
-        this.save();
-    }
-
-    save() {
-        window.DI.localStorage.setItem("DiscordBotsPlugin", JSON.stringify(this.settings));
     }
 
     reactInst(node){
@@ -370,89 +363,53 @@ class DiscordBots extends Plugin {
             this.onUserPopout(user);
         }
     }
+}
 
-    injectOptionHTML() {
-        if($(".app>*:first-child")[0].childNodes.length !== 2) return; // Is in the Options Menu
-        if(!$(".itemDefaultSelected-1UAWLe.item-3879bf.selected-eNoxEK")[0]) return;
-        if($(".itemDefaultSelected-1UAWLe.item-3879bf.selected-eNoxEK").html() !== "Appearance") return; // Is in the Voice Section
-        if($(".db-options")[0]) return; // Don't initiate if it's already there
-        $(".user-settings-appearance>.flex-vertical").append(this.optionHTML);
-        if(this.settings.enabled){
-            $(".db-options input.checkbox-1KYsPm.db-enabled")[0].checked = true;
-            $(".db-options input.checkbox-1KYsPm.db-enabled+.switch-3lyafC").addClass("checked-7qfgSb");
-        }
-        if(this.settings.popout){
-            $(".db-options input.checkbox-1KYsPm.db-popout")[0].checked = true;
-            $(".db-options input.checkbox-1KYsPm.db-popout+.switch-3lyafC").addClass("checked-7qfgSb");
-        }
-        if(this.settings.usedbl){
-            $(".db-options input.db-usedbl")[0].checked = true;
-            $(".db-options input.db-usedbl+.switch-3lyafC").addClass("checked-7qfgSb");
-        }
-        if(this.settings.renderdesc){
-            $(".db-options input.db-renderdesc")[0].checked = true;
-            $(".db-options input.db-renderdesc+.switch-3lyafC").addClass("checked-7qfgSb");
-        }
-        $(".db-options input.inputDefault-Y_U37D").val(this.settings.bdptoken);
-        if(this.optionsInit) return;
-        $(".db-options input.checkbox-1KYsPm.db-enabled").click(()=>{
-            if(this.settings.enabled){
-                $(".db-options input.checkbox-1KYsPm.db-enabled+.switch-3lyafC").removeClass("checked-7qfgSb");
-                this.settings.enabled = false;
-                this.log("Saving 'enabled' as false...");
-                this.save();
-            }else{
-                $(".db-options input.checkbox-1KYsPm.db-enabled+.switch-3lyafC").addClass("checked-7qfgSb");
-                this.settings.enabled = true;
-                this.log("Saving 'enabled' as true...");
-                this.save();
-            }
-        });
-        $(".db-options input.checkbox-1KYsPm.db-popout").click(()=>{
-            if(this.settings.popout){
-                $(".db-options input.checkbox-1KYsPm.db-popout+.switch-3lyafC").removeClass("checked-7qfgSb");
-                this.settings.popout = false;
-                this.log("Saving 'popout' as false...");
-                this.save();
-            }else{
-                $(".db-options input.checkbox-1KYsPm.db-popout+.switch-3lyafC").addClass("checked-7qfgSb");
-                this.settings.popout = true;
-                this.log("Saving 'popout' as true...");
-                this.save();
-            }
-        });
-        $(".db-options input.checkbox-1KYsPm.db-renderdesc").click(()=>{
-            if(this.settings.renderdesc){
-                $(".db-options input.checkbox-1KYsPm.db-renderdesc+.switch-3lyafC").removeClass("checked-7qfgSb");
-                this.settings.renderdesc = false;
-                this.log("Saving 'renderdesc' as false...");
-                this.save();
-            }else{
-                $(".db-options input.checkbox-1KYsPm.db-renderdesc+.switch-3lyafC").addClass("checked-7qfgSb");
-                this.settings.renderdesc = true;
-                this.log("Saving 'renderdesc' as true...");
-                this.save();
-            }
-        });
-        $(".db-options input.db-usedbl").click(()=>{
-            if(this.settings.usedbl){
-                $(".db-options input.db-usedbl+.switch-3lyafC").removeClass("checked-7qfgSb");
-                this.settings.usedbl = false;
-                this.log("Saving 'usedbl' as false...");
-                this.save();
-            }else{
-                $(".db-options input.db-usedbl+.switch-3lyafC").addClass("checked-7qfgSb");
-                this.settings.usedbl = true;
-                this.log("Saving 'usedbl' as true...");
-                this.save();
-            }
-        });
-        $(".db-options input.inputDefault-Y_U37D").change(()=>{
-            this.settings.bdptoken = $(".db-options input.inputDefault-Y_U37D").val();
-            this.log("Saving token...");
-            this.save();
-        });
-        this.optionsInit = true;
+class DiscordBotsSettings extends window.DI.React.Component {
+    render() {
+        return e('div', {},
+            e(SettingsOptionToggle, {
+                title: 'Enabled',
+                lsNode: 'enabled',
+                plugin: this.props.plugin,
+                defaultValue: true
+            }),
+            e(SettingsDivider),
+            e(SettingsOptionToggle, {
+                title: 'Show information in user pop-outs',
+                lsNode: 'popout',
+                plugin: this.props.plugin,
+                defaultValue: false
+            }),
+            e(SettingsDivider),
+            e(SettingsOptionToggle, {
+                title: 'Enable full description rendering',
+                lsNode: 'renderdesc',
+                plugin: this.props.plugin,
+                defaultValue: false
+            }),
+            e(SettingsOptionDescription, {
+                text: 'Descriptions are rendered in a iframe element. This option is turned off by default to lessen loading unneeded assets.'
+            }),
+            e(SettingsDivider),
+            e(SettingsOptionToggle, {
+                title: 'Use DiscordBots.org instead of bots.discord.pw',
+                lsNode: 'usedbl',
+                plugin: this.props.plugin,
+                defaultValue: false
+            }),
+            e(SettingsOptionDescription, {
+                text: '<a href="//discordbots.org" target="_blank">DiscordBots.org</a> uses a similar API as <a href="//bots.discord.pw" target="_blank">bots.discord.pw</a> but adds upvotes, badges for certified developers and bots and does not require a token.'
+            }),
+            e(SettingsDivider),
+            e(SettingsOptionTextbox, {
+                title: 'bots.discord.pw Token',
+                lsNode: 'bdptoken',
+                plugin: this.props.plugin,
+                defaultValue: ''
+            }),
+            e(SettingsDivider)
+        );
     }
 }
 
